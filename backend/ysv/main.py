43535aef0e1a82730da5af167a00904097f83351
huongphamx <huong.px01@gmail.com>
2023-10-02 02:03:49 +0700

    add CORS origin regexes

--- backend/ysv/main.py
+++ backend/ysv/main.py
@@ -5,11 +5,13 @@ from ysv.router_v1 import router as router_v1
 
 app = FastAPI()
 
+origin_regexes = "https://.*\.vercel.app"
 origins = ["http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=origin_regexes,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
