4cf8fb3ee5038ee823e67fc9967e5265c6cae5c4
huongphamx <huong.px01@gmail.com>
2023-10-11 01:15:41 +0700

    fix notification

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   ui: {
     notifications: {
       // Show toasts at the top right of the screen
-      position: 'top-0 bottom-1/2 left-1/2 -translate-x-1/2'
+      position: 'top-0 bottom-auto'
     }
   }
 })
