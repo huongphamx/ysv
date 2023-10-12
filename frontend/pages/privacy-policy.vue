d922e1c7550f523e21b5c06ec145780d5581f737
huongphamx <huong.px01@gmail.com>
2023-10-13 03:08:50 +0700

    add lorem privacy policy

--- frontend/pages/privacy-policy.vue
+++ frontend/pages/privacy-policy.vue
@@ -8,7 +8,72 @@ useHead({
 </script>
 
 <template>
-  <div class="mt-5 mb-24 mycontainer mx-auto">
-    <div>Privacy policy</div>
+  <div class="privacy-body mycontainer mx-auto">
+    <div class="text-medium text-center mb-10">Privacy Policy</div>
+    <p class="text-small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, voluptate. Omnis quisquam
+      recusandae voluptate
+      officia expedita veritatis, labore adipisci? Minus quas, accusantium suscipit pariatur eos a. Quo odit nisi iusto.
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
+    <p class="text-small">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo, neque ut in dolores
+      eius incidunt modi accusamus facere adipisci optio iste at explicabo alias vero quod quas temporibus ipsa!
+    </p>
+    <br>
   </div>
 </template>
+
+
+<style scoped>
+.privacy-body {
+  margin-top: 50px;
+  margin-bottom: 50px;
+
+  @media screen and (min-width: 480px) {
+    margin-bottom: 80px;
+  }
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 100px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    margin-bottom: 150px;
+  }
+}
+
+.text-small {
+  font-size: 16px;
+}
+</style>
