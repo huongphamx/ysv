a22e92bd0950ebe8e45401a98193df277cf30dbb
huongphamx <huong.px01@gmail.com>
2023-10-19 21:57:00 +0700

    update cors for site domain

--- backend/ysv/main.py
+++ backend/ysv/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 
+from ysv.config import common_settings
 from ysv.router_v1 import router as router_v1
 
 app = FastAPI()
 
-origins = ["http://localhost:3000", "https://pxh-dev.online"]
+origins = ["http://localhost:3000", common_settings.FRONTEND_DOMAIN]
 
 app.add_middleware(
     CORSMiddleware,
